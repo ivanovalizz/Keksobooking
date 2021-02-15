25da4e492a84d81140e01e9d352eff98f2a07feb
ivanovalizz <elizaveta.sm97@gmail.com>
2021-02-15 10:58:04 +0300

    Создаёт фрагмент с 10 DOM-элементами и отрисовывает первый элемент на карте

--- js/main.js
+++ js/main.js
@@ -2,4 +2,28 @@ import {fetchAds} from './mocks.js';
 
 const randomCountOfAds = 10;
 
-console.log(fetchAds(randomCountOfAds));
+const template = document.querySelector('#card')
+  .content
+  .querySelector('article');
+
+const fragment = document.createDocumentFragment();
+
+fetchAds(randomCountOfAds).forEach((ad) => {
+  const elementAd = template.cloneNode(true);
+  elementAd.querySelector('.popup__title').textContent = ad.offer.title;
+  elementAd.querySelector('.popup__text--address').textContent = ad.offer.address;
+  elementAd.querySelector('.popup__text--price').textContent = ad.offer.price + ' ₽/ночь';
+  elementAd.querySelector('.popup__text--capacity').textContent = ad.offer.rooms + ' комнаты для ' + ad.offer.guests + ' гостей';
+  elementAd.querySelector('.popup__text--time').textContent = 'Заезд после ' + ad.offer.checkin + ', выезд до ' + ad.offer.checkout;
+  elementAd.querySelector('.popup__features').textContent = ad.offer.features[0];
+  elementAd.querySelector('.popup__description').textContent = ad.offer.description;
+  elementAd.querySelector('.popup__avatar').src = ad.author.avatar;
+
+  fragment.appendChild(elementAd);
+});
+console.dir(fragment)
+document.querySelector('#map-canvas').appendChild(fragment.firstElementChild);
+
+/*const elementCard = template.cloneNode(true);
+const checkAnElementContainsAValue = (item, value) => !value ? item.style.display = 'none': null;*/
+
