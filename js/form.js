13c1bfdb9c7ff187cfc86396353ff8641a184c1c
ivanovalizz <elizaveta.sm97@gmail.com>
2021-02-20 22:56:37 +0300

    Рефакторит код

--- js/form.js
+++ js/form.js
@@ -3,19 +3,27 @@ const typeInputElement = formElement.querySelector('#type')
 const priceInputElement = formElement.querySelector('#price');
 
 typeInputElement.addEventListener('change', function() {
-  if (typeInputElement.value === 'palace') {
-    priceInputElement.min = '10000';
-    priceInputElement.placeholder = '10000';
-  } else if (typeInputElement.value === 'house') {
-    priceInputElement.min = '5000';
-    priceInputElement.placeholder = '5000';
-  } else if (typeInputElement.value === 'flat') {
-    priceInputElement.min = '1000';
-    priceInputElement.placeholder = '1000';
-  } else {
-    priceInputElement.min = '0';
-    priceInputElement.placeholder = '0';
-  }
+  const minPrice = {
+    palace: {
+      min: '10000',
+      placeholder: '10000',
+    },
+    house: {
+      min: '5000',
+      placeholder: '5000',
+    },
+    flat: {
+      min: '1000',
+      placeholder: '1000',
+    },
+    bungalow: {
+      min: '0',
+      placeholder: '0',
+    },
+  };
+
+  priceInputElement.min = minPrice[typeInputElement.value].min;
+  priceInputElement.placeholder = minPrice[typeInputElement.value].placeholder;
 });
 
 const checkinInputElement = formElement.querySelector('#timein');
