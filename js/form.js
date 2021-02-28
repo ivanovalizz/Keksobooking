e5e503f75a09fea700a08d86c415089e29db622b
ivanovalizz <elizaveta.sm97@gmail.com>
2021-02-28 23:21:06 +0300

    Добавляет валидацию на поля «Количество гостей» и «Количество комнат»

--- js/form.js
+++ js/form.js
@@ -36,3 +36,42 @@ const onCheckinCheckoutChange = function () {
 
 checkinInputElement.addEventListener('change', onCheckinCheckoutChange);
 checkoutInputElement.addEventListener('change', onCheckinCheckoutChange);
+
+const roomNumberElement = formElement.querySelector('#room_number');
+const capacityElement = formElement.querySelector('#capacity');
+
+const onRoomNumberAndCapacitySelectChange = function () {
+  if (roomNumberElement.value === '1') {
+    if (capacityElement.value === '2' || capacityElement.value === '3' || capacityElement.value === '0') {
+      capacityElement.setCustomValidity('Выбранное количество комнат не подходит для такого количества гостей');
+    } else {
+      capacityElement.setCustomValidity('');
+    }
+  }
+  if (roomNumberElement.value === '2') {
+    if (capacityElement.value === '3' || capacityElement.value === '0') {
+      capacityElement.setCustomValidity('Выбранное количество комнат не подходит для такого количества гостей');
+    } else {
+      capacityElement.setCustomValidity('');
+    }
+  }
+  if (roomNumberElement.value === '3') {
+    if (capacityElement.value === '0') {
+      capacityElement.setCustomValidity('Выбранное количество комнат подходит только для гостей');
+    } else {
+      capacityElement.setCustomValidity('');
+    }
+  }
+  if (roomNumberElement.value === '100') {
+    if (capacityElement.value === '1' || capacityElement.value === '2' || capacityElement.value === '3') {
+      capacityElement.setCustomValidity('Выбранное количество комнат не подходит для гостей');
+    } else {
+      capacityElement.setCustomValidity('');
+    }
+  }
+
+  capacityElement.reportValidity();
+}
+
+roomNumberElement.addEventListener('change', onRoomNumberAndCapacitySelectChange)
+capacityElement.addEventListener('change', onRoomNumberAndCapacitySelectChange)
