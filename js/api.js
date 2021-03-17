5cb7a648b9a52b90f419375ccb6d931623df4b7b
ivanovalizz <elizaveta.sm97@gmail.com>
2021-03-17 17:23:40 +0300

    Рефакторит код

--- js/api.js
+++ js/api.js
@@ -4,9 +4,7 @@ export const fetchSimilarAds = (onSuccess, onFail) => {
   fetch('https://22.javascript.pages.academy/keksobooking/data')
     .then((response) => response.json())
     .then((data) => onSuccess(data))
-    .catch((error) => {
-      console.log(error)
-      onFail(errorMessage)
+    .catch(() => {onFail(errorMessage)
     });
 };
 
