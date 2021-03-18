f2d7f22c092abb61324871f8a7d627830f52c79b
ivanovalizz <elizaveta.sm97@gmail.com>
2021-03-18 22:43:01 +0300

    Правит код по замечаниям проверяющего

--- source/js/card.js
+++ source/js/card.js
@@ -13,21 +13,37 @@ const setElementParam = (element, value, param = 'textContent') => {
   }
 }
 
-const createPhotosFragment = (imageTemplate, photos) => {
+const initPhotos = (photosElement, photos) => {
+  const imageTemplate = photosElement.querySelector('img')
   const imagesFragment = document.createDocumentFragment()
 
-  for (let i = 0; i < photos.length; i++) {
+  photos.forEach(photo => {
     const img = imageTemplate.cloneNode(true)
-    img.src = photos[i]
+    img.src = photo
     imagesFragment.appendChild(img)
-  }
+  })
 
-  return imagesFragment
+  photosElement.innerHTML = ''
+  photosElement.appendChild(imagesFragment)
+}
+
+const initFeatures = (el, features) => {
+  const featuresFragment = document.createDocumentFragment()
+
+  features.forEach(feature => {
+    const template = document.createElement('li')
+    template.classList.add('popup__feature')
+    template.classList.add(`popup__feature--${feature}`)
+    featuresFragment.appendChild(template)
+  })
+
+  el.innerHTML = ''
+  el.appendChild(featuresFragment)
+
 }
 
 export const getCardElement = point => {
-  const template = document.querySelector('#card').content.querySelector('article')
-  const cardElement = template.cloneNode(true)
+  const cardElement = document.querySelector('#card').content.querySelector('.popup').cloneNode(true)
 
   setElementParam(cardElement.querySelector('.popup__title'), point.offer.title)
   setElementParam(cardElement.querySelector('.popup__text--address'), point.offer.address)
@@ -35,15 +51,11 @@ export const getCardElement = point => {
   setElementParam(cardElement.querySelector('.popup__type'), OFFER_TYPES_DICTIONARY[point.offer.type])
   setElementParam(cardElement.querySelector('.popup__text--capacity'), `${point.offer.rooms} комнаты для ${point.offer.guests} гостей`)
   setElementParam(cardElement.querySelector('.popup__text--time'), `Заезд после  ${point.offer.checkin}, выезд до ${point.offer.checkout}`)
-  setElementParam(cardElement.querySelector('.popup__features'), point.offer.features.join(', '))
   setElementParam(cardElement.querySelector('.popup__description'), point.offer.description)
   setElementParam(cardElement.querySelector('.popup__avatar'), point.author.avatar, 'src')
 
-  const photosElement = cardElement.querySelector('.popup__photos')
-  const imagesFragment = createPhotosFragment(photosElement.querySelector('img'), point.offer.photos)
-
-  photosElement.innerHTML = ''
-  photosElement.appendChild(imagesFragment)
+  initFeatures(cardElement.querySelector('.popup__features'), point.offer.features)
+  initPhotos(cardElement.querySelector('.popup__photos'), point.offer.photos)
 
   return cardElement
 }
--- source/js/utils.js
+++ source/js/utils.js
@@ -1,1 +1,9 @@
+import {DEFAULT_MAIN_MARKER_LOCATION} from './map';
+
+const FRACTION_DIGIT = 5
+
 export const isEscEvent = evt => evt.key === 'Escape' || evt.key === 'Esc'
+export const getAddressString = (
+  lat = DEFAULT_MAIN_MARKER_LOCATION.lat,
+  lng = DEFAULT_MAIN_MARKER_LOCATION.lng,
+) => `${lat.toFixed(FRACTION_DIGIT)}, ${lng.toFixed(FRACTION_DIGIT)}`
